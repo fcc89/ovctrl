6956f1eb1f569d72c4d4ba99bf3527a28c6ad5ff
fcc89 <fg.ccalderon@gmail.com>
2019-09-29 23:13:56 +0200

    added connections to ucontroller

---
+++ firmware/testPICconfig/firmware/pintest.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# perifTest configuration
+CND_ARTIFACT_DIR_perifTest=dist/perifTest/production
+CND_ARTIFACT_NAME_perifTest=pintest.X.production.hex
+CND_ARTIFACT_PATH_perifTest=dist/perifTest/production/pintest.X.production.hex
+CND_PACKAGE_DIR_perifTest=${CND_DISTDIR}/perifTest/package
+CND_PACKAGE_NAME_perifTest=pintest.x.tar
+CND_PACKAGE_PATH_perifTest=${CND_DISTDIR}/perifTest/package/pintest.x.tar
